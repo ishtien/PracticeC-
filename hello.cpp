bdb00387919ca13031f13a6ee176cf818c669ccd
ishtien <203isthebest@gmail.com>
2024-10-22 22:53:36 +0800

    Update hello.cpp remotely.

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello world!world world" << endl;
+    cout << "Hello world!world world world" << endl;
     cout << "Hello world!world world" << endl;
     cout << "Hello world! Hi Hi" << endl;
     cout << "Hello world! Hi Hi" << endl;
