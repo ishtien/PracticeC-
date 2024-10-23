1816d42e2a2b5b08b00726a96297e804e33c49aa
ishtien <203isthebest@gmail.com>
2024-10-23 13:37:08 +0800

    Change hello.cpp.

--- hello.cpp
+++ hello.cpp
@@ -5,4 +5,5 @@ int main(){
     cout << "Hello world!world world" << endl;
     cout << "Hello world! Hi Hi" << endl;
     cout << "Hello world! Hi Hi hi" << endl;
+    cout << "Hello world! Hi Hi" << endl;
 }
